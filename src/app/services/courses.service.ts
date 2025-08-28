a63ff0bf514716478ed5c6ed9f609331fb4accae
Ängla Juliusson <angla02@icloud.com>
2025-08-28 10:55:33 +0200

    Anrop

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,13 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-   // Properties
-   private url: string = "public/ramschema.json";
+  // Properties
+  private url: string = "public/ramschema.json";
 
   constructor(private http: HttpClient) { }
+
+
+  // Methods
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.url);
+  }
 }

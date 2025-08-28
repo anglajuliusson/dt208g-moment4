dc489f2f0f93bfcd159118f7be079a7f89297e0b
Ängla Juliusson <angla02@icloud.com>
2025-08-28 11:09:47 +0200

    Ta bort home och ersätt med courses

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: 'courses', component: HomeComponent },
+    { path: 'courses', component: CoursesComponent },
     { path: '', redirectTo: '/courses', pathMatch: 'full'},
     { path: '404', component: NotFoundComponent },
     { path: '**', component: NotFoundComponent },

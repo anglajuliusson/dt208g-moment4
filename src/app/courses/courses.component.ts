1d22e16d68ccb2b3db1410b98868f2e431c872a3
Ängla Juliusson <angla02@icloud.com>
2025-08-29 09:22:55 +0200

    Funktionalitet för sökruta och sortering

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,23 +1,56 @@
 import { Component } from '@angular/core';
-import { CoursesService } from '../services/courses.service';
-import { Course } from '../model/course';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule, NgFor, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
-  courselist: Course[] = [];
+  courselist: Course[] = []; // Orginaldata
+  sortedCourseList: Course[] = []; // Sorterade listan
+  search: string = '';
+  sortCourseList: boolean = true; // Stigande, fallande
 
-  constructor(private courseservice : CoursesService) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.courseservice.getCourses().subscribe(data => {
-      this.courselist = data;
-    })
+    this.loadCourses();
+  }
+
+  loadCourses() {
+    this.http.get<Course[]>('assets/ramschema.json').subscribe({
+      next: (data) => {
+        this.courselist = data;
+        this.sortedCourseList = [...data]; // Kopiera initialt
+      },
+      error: () => {
+        console.error('Fel vid hämtning av data');
+      }
+    });
+  }
+
+  // Filtrerar data direkt när användaren skriver
+  filterData() {
+    const phrase = this.search.toLowerCase();
+    this.sortedCourseList = this.courselist.filter(course =>
+      course.code.toLowerCase().includes(phrase) ||
+      course.coursename.toLowerCase().includes(phrase)
+    );
+  }
+
+  // Sortera efter valfritt fält
+  sortBy(field: keyof Course) {
+    this.sortCourseList = !this.sortCourseList;
+    this.sortedCourseList.sort((a, b) => {
+      const valueA = a[field].toLowerCase();
+      const valueB = b[field].toLowerCase();
+      return this.sortCourseList ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+    });
   }
 }
